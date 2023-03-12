af8e297a0d62e35d3c51c4fd0ffa9930890f88f6
Conjuction <suhinin-d@mail.ru>
2023-03-12 19:32:15 +0500

    added config

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -8,17 +8,21 @@ import org.aeonbits.owner.Config;
 
 public interface WebDriverConfig extends Config {
     @Key("baseUrl")
+    @DefaultValue("https://store.steampowered.com/")
     String getBaseUrl();
 
     @Key("browserName")
+    @DefaultValue("chrome")
     String getBrowser();
 
     @Key("browserVersion")
+    @DefaultValue("100.0")
     String getBrowserVersion();
 
     @Key("remoteUrl")
     String getRemoteUrl();
 
     @Key("browserSize")
+    @DefaultValue("1920x1080")
     String getBrowserSize();
 }
