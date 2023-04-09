967d8f5e4eb6b60fe2fe9e05e12cf744eb1c41d2
Conjuction <suhinin-d@mail.ru>
2023-04-09 15:29:04 +0500

    add read.me

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -10,7 +10,6 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static io.qameta.allure.Allure.step;
 
 public class MainPage {
     private final SelenideElement inputSearch =  $("#store_nav_search_term");
